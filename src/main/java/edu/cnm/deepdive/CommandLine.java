6303638d9c4aeff614e8c49b7a18caef0457882a
Jonathan Rodrigues <jonrodrigues24@gmail.com>
2021-06-07 11:42:44 -0600

    Implemented simple program to read command line

---
+++ src/main/java/edu/cnm/deepdive/CommandLine.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive;
+
+public class CommandLine {
+
+	public static void main(String[] args) {
+	
+		for (String each : args) {
+			
+			System.out.println(each.toUpperCase());
+			
+		}
+	
+	}
+	
+}
